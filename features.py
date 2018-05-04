e774d9f33f75b113a7997b0f67c82e921eaa372d
Paul Choi <choipaul96@gmail.com>
2018-05-04 05:29:55 -0500

    implement most of the features

--- features.py
+++ features.py
@@ -1,25 +1,78 @@
+import math
 from textblob import TextBlob
+from bs4 import BeautifulSoup
 
 class Feature(object):
-    def score(self, submission):
+    @staticmethod
+    def weight_by_upvotes(comments):
+        '''Gives comments and weights by relative number of upvotes, i.e.
+        score / sum.'''
+        total = sum(comment.score for comment in comments)
+        return (comment.score / total for comment in comments)
+
+    @staticmethod
+    def weighted_comment_score(weights, comments, score_comment):
+        return (weight * score_comment(comment)
+                for weight, comment in zip(weights, comments))
+
+    @staticmethod
+    def sigmoid(x, low=-1, high=1):
+        '''Applies a sigmoid to values bounded in a given range to "boost"
+        values closer to the center of the range'''
+        delta = (high - low) / 2
+        center = (high + low) / 2
+        _sigmoid = lambda x: x / (1 + abs(x))
+        return _sigmoid(x - center) * (delta / _sigmoid(delta)) + center
+
+    def score_submission(self, submission):
+        submission.comments.replace_more(limit=0)
+        comments = submission.comments.list()
+        return self.score_comments(comments)
+
+    def score_comments(self, comments):
         raise NotImplementedError
 
 class Polarity(Feature):
-    def score(self, submission):
-        raise NotImplementedError
+    @staticmethod
+    def compute_polarity(comment):
+        return Feature.sigmoid(TextBlob(comment.body).sentiment.polarity)
+
+    def score_comments(self, comments):
+        weights = self.weight_by_upvotes(comments)
+        return sum(self.weighted_comment_score(
+            weights, comments, self.compute_polarity))
 
 class Subjectivity(Feature):
-    def score(self, submission):
-        raise NotImplementedError
+    @staticmethod
+    def compute_subjectivity(comment):
+        return Feature.sigmoid(TextBlob(comment.body).sentiment.subjectivity, low=0, high=1)
+
+    def score_comments(self, comments):
+        weights = self.weight_by_upvotes(comments)
+        return sum(self.weighted_comment_score(
+            weights, comments, self.compute_subjectivity))
 
 class PoliticalAffiliation(Feature):
     def score(self, submission):
         raise NotImplementedError
 
 class LinkCount(Feature):
-    def score(self, submission):
-        raise NotImplementedError
+    @staticmethod
+    def count_links(comment):
+        soup = BeautifulSoup(comment.body_html, 'html.parser')
+        return sum(1 for _ in soup.findAll('a'))
+
+    def score_comments(self, comments):
+        weights = self.weight_by_upvotes(comments)
+        return sum(self.weighted_comment_score(
+            weights, comments, self.count_links))
 
 class CommentLength(Feature):
-    def score(self, submission):
-        raise NotImplementedError
+    @staticmethod
+    def count_words(comment):
+        return len(comment.body.split())
+
+    def score_comments(self, comments):
+        weights = self.weight_by_upvotes(comments)
+        return sum(self.weighted_comment_score(
+            weights, comments, self.count_words))
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import features
 import praw
 import secrets
 
@@ -7,6 +8,12 @@ def main():
     reddit = praw.Reddit(client_id=secrets.client_id,
                          client_secret=secrets.client_secret,
                          user_agent=user_agent)
+
+    # submission = reddit.submission(url='https://www.reddit.com/r/netneutrality/comments/7iym1b/this_is_my_senator_pat_toomey_he_sold_me_and_my/')
+    # submission = reddit.submission(url='https://www.reddit.com/r/philosophy/comments/8624sp/a_death_row_inmates_dementia_means_he_cant/')
+    submission = reddit.submission(url='https://www.reddit.com/r/philosophy/comments/5u78oy/on_this_day_february_15_2416_years_ago_socrates/')
+    score = features.CommentLength().score_submission(submission)
+    print(score)
 
 if __name__ == '__main__':
     main()
