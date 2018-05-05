967236db4c58b8e8a9f46bd31872af69cd8ed4d9
Paul Choi <choipaul96@gmail.com>
2018-05-04 19:21:10 -0500

    remove comments

--- main.py
+++ main.py
@@ -22,9 +22,5 @@ def main():
         score = feat().score_comments(submission.comments)
         print('{} score: {}'.format(feat.__name__, score))
 
-    # submission = reddit.submission(url='https://www.reddit.com/r/netneutrality/comments/7iym1b/this_is_my_senator_pat_toomey_he_sold_me_and_my/')
-    # submission = reddit.submission(url='https://www.reddit.com/r/philosophy/comments/8624sp/a_death_row_inmates_dementia_means_he_cant/')
-    # submission = reddit.submission(url='https://www.reddit.com/r/philosophy/comments/5u78oy/on_this_day_february_15_2416_years_ago_socrates/')
-
 if __name__ == '__main__':
     main()
